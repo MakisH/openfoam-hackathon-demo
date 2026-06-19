6d3fdfc8604ab56480419a8aa4c5522a51e5db2f
Gerasimos Chourdakis <gerasimos.chourdakis@ipvs.uni-stuttgart.de>
2026-06-19 13:20:28 +0200

    Update exe3.C

--- exe3.C
+++ exe3.C
@@ -90,7 +90,7 @@ int main(int argc, char *argv[])
             << nl;
     }
 
-    Info << "XXXXXXXXXXXXXXXX" << endl;
+    Info << "XXXXXXXXXXXXXXXXXXXXX" << endl;
 
     labelList faceCells({3, 9, 3, 14, 9, 9, 25, 14});
 
